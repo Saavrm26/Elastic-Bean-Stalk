603aad383298f31cb8f6cc4f981549164af0b949
Saarthak Verma <saarthakverma739@gmail.com>
2022-06-29 16:02:46 +0530

    some more changes

--- lib/Screen/LoginScreen/loginScreen.dart
+++ lib/Screen/LoginScreen/loginScreen.dart
@@ -10,122 +10,93 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  bool isSuccessful = false;
+
   bool isRun = false;
-//   String getAuthenticate = """
-//     mutation
-//     {
-// 	    Authenticate(input:{\$emailOrPhone:String!,\$password:String!})
-//       {
-//         token
-//       }
-//     }
-// """;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "LOGIN",
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-            Form(
+      body: Mutation(
+          options: MutationOptions(
+            document: gql(
+                """
+                        mutation
+                        {
+                          Authenticate(input:{emailOrPhone:\$emailOrPhone,password:\$password})
+                          {
+                            token
+                          }
+                        }
+                      """),
+            update:
+                (GraphQLDataProxy cache, QueryResult<Object?>? result) {
+              print("inside update");
+              return cache;
+            },
+            onCompleted: (dynamic resultData) {
+              print("inside on completed ");
+            },
+          ),
+          builder: (RunMutation runMutation, QueryResult<Object?>? result) {
+            print(result?.data?.toString());
+            return Center(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    margin: const EdgeInsets.all(10),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                          hintText: "Enter email or username",
-                          filled: true,
-                          // fillColor: ,
-                          enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 3, color: Colors.black))),
-                      autocorrect: false,
+                  Text(
+                    "LOGIN",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .displayLarge,
+                  ),
+                  Form(
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                                hintText: "Enter email or username",
+                                filled: true,
+                                // fillColor: ,
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                    BorderSide(width: 3, color: Colors.black))),
+                            autocorrect: false,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                                hintText: "Enter password",
+                                filled: true,
+                                // fillColor: Colors.green,
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                    BorderSide(width: 3, color: Colors.black))),
+                            autocorrect: false,
+                          ),
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              runMutation(<String, dynamic>{
+                                'emailOrPhone': '9456679268',
+                                'password': 'goodguy123'
+                              });
+                              print(result?.data);
+                              // major modifications from the last commit
+                            },
+                            child: const Text("click me")
+                        )
+                      ],
                     ),
                   ),
-                  Container(
-                    margin: const EdgeInsets.all(10),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                          hintText: "Enter password",
-                          filled: true,
-                          // fillColor: Colors.green,
-                          enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 3, color: Colors.black))),
-                      autocorrect: false,
-                    ),
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          print('inside set state');
-                          isRun = true;
-                        });
-                      },
-                      child: const Text("click me")
-                  )
                 ],
               ),
-            ),
-            (() {
-              print("inside function at 78");
-              print(isRun);
-
-            if (isRun==true) {
-              isRun=false;
-              print('inside 81');
-            // if(isSuccessful){
-            //   Navigator.of(context).pushNamed(RouteManager.home, arguments: {});
-            // }
-              return Mutation(
-                options: MutationOptions(
-                  document: gql(
-                      """
-                      mutation
-                      {
-                        Authenticate(input:{emailOrPhone:\$emailOrPhone,password:\$password})
-                        {
-                          token
-                        }
-                      }
-                    """),
-                  update:
-                      (GraphQLDataProxy cache, QueryResult<Object?>? result) {
-                        print("inside update");
-                        return cache;
-                      },
-                  onCompleted: (dynamic resultData) {
-                    print("inside on completed ");
-                  },
-                ),
-                builder: (RunMutation runMutation, QueryResult<Object?>? result) {
-                  print(runMutation({'emailOrPhone':'9456679268','password':'goodguy123'}));
-                  print(result?.data);
-                  // if ((result as Map).containsKey('errors')) {
-                  //   isSuccessful = false;
-                  // } else {
-                  //   isSuccessful = true;
-                  //   Navigator.of(context).pushNamed(RouteManager.home, arguments: {});
-                  // }
-                  return Container();
-                },
-              );
-
-            }
-            else{
-              return Container();
-              }
-            }
-            ()),
-          ],
-        ),
+            );
+          }
       ),
     );
   }
