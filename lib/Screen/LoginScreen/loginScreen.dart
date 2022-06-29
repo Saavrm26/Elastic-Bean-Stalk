a238559d582371783141d2e7cce0bd3c86f95e3d
Saarthak Verma <saarthakverma739@gmail.com>
2022-06-29 14:15:12 +0530

    login changes and some errors

--- lib/Screen/LoginScreen/loginScreen.dart
+++ lib/Screen/LoginScreen/loginScreen.dart
@@ -1,58 +1,132 @@
+import 'package:elastic_bean_stalk/Utils/Routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isSuccessful = false;
+  bool isRun = false;
+//   String getAuthenticate = """
+//     mutation
+//     {
+// 	    Authenticate(input:{\$emailOrPhone:String!,\$password:String!})
+//       {
+//         token
+//       }
+//     }
+// """;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        // color: Colors.red ,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "LOGIN",
-                style: Theme.of(context).textTheme.displayLarge,
-              ),
-              Form(
-                  child: Column(
-                    children:  [
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        child: const TextField(
-                          decoration:
-                          InputDecoration(
-                              hintText: "Enter email or username",
-                              filled: true,
-                              // fillColor: ,
-                              enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 3,color: Colors.black))
-                          ),
-                          autocorrect: false,
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        child: const TextField(
-                          decoration:
-                          InputDecoration(
-                              hintText: "Enter password",
-                              filled: true,
-                              // fillColor: Colors.green,
-                              enabledBorder: OutlineInputBorder(borderSide: BorderSide(width: 3,color: Colors.black))
-                          ),
-                          autocorrect: false,
-                        ),
-                      )
-                    ],
-
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "LOGIN",
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            Form(
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          hintText: "Enter email or username",
+                          filled: true,
+                          // fillColor: ,
+                          enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(width: 3, color: Colors.black))),
+                      autocorrect: false,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          hintText: "Enter password",
+                          filled: true,
+                          // fillColor: Colors.green,
+                          enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(width: 3, color: Colors.black))),
+                      autocorrect: false,
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          print('inside set state');
+                          isRun = true;
+                        });
+                      },
+                      child: const Text("click me")
                   )
+                ],
               ),
-            ],
-          ),
+            ),
+            (() {
+              print("inside function at 78");
+              print(isRun);
+
+            if (isRun==true) {
+              isRun=false;
+              print('inside 81');
+            // if(isSuccessful){
+            //   Navigator.of(context).pushNamed(RouteManager.home, arguments: {});
+            // }
+              return Mutation(
+                options: MutationOptions(
+                  document: gql(
+                      """
+                      mutation
+                      {
+                        Authenticate(input:{emailOrPhone:\$emailOrPhone,password:\$password})
+                        {
+                          token
+                        }
+                      }
+                    """),
+                  update:
+                      (GraphQLDataProxy cache, QueryResult<Object?>? result) {
+                        print("inside update");
+                        return cache;
+                      },
+                  onCompleted: (dynamic resultData) {
+                    print("inside on completed ");
+                  },
+                ),
+                builder: (RunMutation runMutation, QueryResult<Object?>? result) {
+                  print(runMutation({'emailOrPhone':'9456679268','password':'goodguy123'}));
+                  print(result?.data);
+                  // if ((result as Map).containsKey('errors')) {
+                  //   isSuccessful = false;
+                  // } else {
+                  //   isSuccessful = true;
+                  //   Navigator.of(context).pushNamed(RouteManager.home, arguments: {});
+                  // }
+                  return Container();
+                },
+              );
+
+            }
+            else{
+              return Container();
+              }
+            }
+            ()),
+          ],
         ),
       ),
-    );;
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,50 @@ import 'package:elastic_bean_stalk/Utils/Routes/routes.dart';
 import 'package:elastic_bean_stalk/Utils/Theme/theme.dart';
 
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+final HttpLink httpLink = HttpLink(
+    'http://humapp-env.eba-5psj3xci.ap-south-1.elasticbeanstalk.com/graphql',
+);
+
+ ValueNotifier<GraphQLClient> client = ValueNotifier(
+  GraphQLClient(
+    link: httpLink,
+    // The default store is the InMemoryStore, which does NOT persist to disk
+    cache: GraphQLCache(store: HiveStore()),
+  ),
+);
+
+void main() async{
+  await initHiveForFlutter();
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+class MyApp extends StatefulWidget {
+  // late ValueNotifier<GraphQLClient> client;
+  MyApp({Key? key}) : super(key: key);
+  // MyApp.initClient(this.client,{Key? key}) : super(key: key);
+  // ValueNotifier<GraphQLClient> getClient(){
+  //   return client;
+  // }
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // ValueNotifier<GraphQLClient> client=MyApp().client;
+  _MyAppState();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
+    return GraphQLProvider(
+      client: client,
+       child:MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: RouteManager.signup,
+      initialRoute: RouteManager.login,
       theme: (CustomTheme.isDark) ? CustomTheme.darkTheme() : CustomTheme.lightTheme(),
       onGenerateRoute: RouteManager.generateRoute,
+    )
     );
   }
 }
